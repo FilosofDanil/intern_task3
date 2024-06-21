0c04e244fe9e681b5171d8d2bc5c405cdbeafe52
danylo <serhio3347@gmail.com>
2024-06-21 18:17:12 +0300

    added auth handling

--- src/pages/profile/containers/Profile.jsx
+++ src/pages/profile/containers/Profile.jsx
@@ -1,10 +1,12 @@
 import useTheme from "../../../misc/hooks/useTheme";
 import {useIntl} from "react-intl";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import exportFunctions from "../actions/profile";
 import Typography from "../../../components/Typography";
 import {createUseStyles} from "react-jss";
+import pagesURLs from "../../../constants/pagesURLs";
+import * as pages from "../../../constants/pages";
 
 const getClasses = createUseStyles((theme) => ({
 
@@ -15,18 +17,36 @@ const ProfilePage = () => {
     const classes = getClasses({theme});
     const {formatMessage} = useIntl();
     const dispatch = useDispatch();
+    const [isAuth, setIsAuth] = useState(true);
     const profile = useSelector((state) => state.profile);
 
     useEffect(() => {
         dispatch(exportFunctions.fetchProfile())
-
+        checkProfile()
     }, [])
+
+    const checkProfile = () => {
+       if(!profile){
+           setIsAuth(false)
+       } else {
+           setIsAuth(true);
+       }
+    };
 
     return (
         <div>
-            <Typography variant="title"> {formatMessage({id: 'title'})}</Typography>
-            <Typography variant="title"> {profile.name}</Typography>
-            <Typography variant="title"> {profile.email}</Typography>
+            {!isAuth && (
+                <div>
+                    <Typography variant="title"> {formatMessage({id: 'title'})}</Typography>
+                </div>
+            )}
+            {isAuth && (
+                <div>
+                    <Typography variant="title"> {formatMessage({id: 'title'})}</Typography>
+                    <Typography variant="title"> {profile.name}</Typography>
+                    <Typography variant="title"> {profile.email}</Typography>
+                </div>
+            )}
         </div>
     );
 }
