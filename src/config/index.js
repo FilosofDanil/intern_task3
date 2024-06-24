fdf70cb215685af0876839a80681a39db318f742
FilosofDanil <serhio3347@gmail.com>
2024-06-24 13:08:09 +0200

    changed service uri

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   // Services
   USERS_SERVICE: 'http://localhost:3000',
-  BACKEND_SERVICE: 'https://serene-intuition-production.up.railway.app/',
+  BACKEND_SERVICE: 'https://serene-intuition-production.up.railway.app',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
