3a445e0feb5f02a8843159ff42b0c8dd88fb9fb4
danylo <serhio3347@gmail.com>
2024-06-27 11:36:05 +0300

    changed backend link

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const config = {
   // Services
   USERS_SERVICE: 'http://localhost:3000',
-  // BACKEND_SERVICE: 'https://serene-intuition-production.up.railway.app',
-  BACKEND_SERVICE: 'http://localhost:1000',
+  BACKEND_SERVICE: 'https://serene-intuition-production.up.railway.app',
+  // BACKEND_SERVICE: 'http://localhost:1000',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
